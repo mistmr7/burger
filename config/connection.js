69054c7c2ef5d782c263ef2bacb450ff94487384
mistmr7 <michael.mistarz@gmail.com>
2018-09-11 19:21:49 -0400

    commit syntax fix

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection 
+var connection;
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL)
@@ -13,6 +13,7 @@ if (process.env.JAWSDB_URL) {
   password: "root",
   database: "burger_db"
 });
+}
 
 // Make connection.
 connection.connect(function(err) {
